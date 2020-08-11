98ebf5c81eebcec2bdd121902a5ae620b5c22e45
Gabriel <gabo6252@gmail.com>
2020-08-11 11:05:54 -0400

    added a coment

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 
+//start express app
 const app = express();
 
 app.set('view engine', 'pug');
